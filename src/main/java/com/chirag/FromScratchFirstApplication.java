66da75881a9c58dd8d6b78aa5d4902e333b1b484
Chirag Goyal <chirag.goyal@freecharge.com>
2018-06-13 10:04:39 +0530

    Second commit

---
+++ src/main/java/com/chirag/FromScratchFirstApplication.java
@@ -0,0 +1,12 @@
+package com.chirag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromScratchFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromScratchFirstApplication.class, args);
+	}
+}
